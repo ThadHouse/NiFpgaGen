a5335dad64233e2070456abeaa1fbb6185ea6e2a
Thad House <thadhouse1@gmail.com>
2020-08-23 00:49:44 -0700

    Initial commit of FRC FPGA generator

---
+++ src/NiFpgaGen/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NiFpgaGen
+{
+    public enum DataType
+    {
+        Array,
+        Boolean,
+        Cluster,
+        FXP,
+        EnumU16,
+        U8,
+        I8,
+        I16,
+        U16,
+        I32,
+        U32,
+        U64,
+        I64
+    }
+
+    public class Register
+    {
+        public string Name { get; set; }
+        public bool Hidden { get; }
+        public bool Indicator { get; }
+        public DataType DataType { get; }
+        public uint Offset { get; }
+        public XElement ExtraData { get; }
+
+        public List<(int, uint)> OffsetList = new List<(int, uint)>();
+
+        public Register(XElement element)
+        {
+            Name = element.Element("Name").Value;
+            Hidden = bool.Parse(element.Element("Hidden").Value);
+            Indicator = bool.Parse(element.Element("Indicator").Value);
+            DataType = Enum.Parse<DataType>(((XElement)element.Element("Datatype").FirstNode).Name.LocalName);
+            Offset = uint.Parse(element.Element("Offset").Value);
+            ExtraData = element;
+            ;
+        }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string fileName = @"C:\Users\thadh\Documents\GitHub\thadhouse\NiFpgaGen\roboRIO_FPGA_2020_20.1.2.lvbitx";
+
+            using var file = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+
+            XElement bitfile = await XElement.LoadAsync(file, LoadOptions.None, CancellationToken.None);
+
+            var registerList = bitfile.Descendants("VI").Descendants("RegisterList").Descendants("Register").Select(x => new Register(x));
+
+            var frcMapping = new FRCMapping(registerList);
+
+            frcMapping.GenerateC(@"C:\Users\thadh\Documents\GitHub\thadhouse\NiFpgaGen\Generated", "FRC");
+
+            ;
+        }
+    }
+}
---
+++ src/NiFpgaGen/SourceCodeStringBuilder.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiFpgaGen
+{
+    public class SourceCodeStringBuilder
+    {
+        private StringBuilder builder = new StringBuilder();
+        private string currentIndent;
+        private string stepsPerIndent;
+
+        public SourceCodeStringBuilder(int stepsPerIndent)
+        {
+            this.stepsPerIndent = "";
+            for (int i = 0; i < stepsPerIndent; i++)
+            {
+                this.stepsPerIndent += " ";
+            }
+            this.currentIndent = "";
+        }
+
+        public SourceCodeStringBuilder Clear()
+        {
+            builder.Clear();
+            return this;
+        }
+
+        public SourceCodeStringBuilder AppendIndent()
+        {
+            builder.Append(currentIndent);
+            return this;
+        }
+
+        public SourceCodeStringBuilder Append(string value)
+        {
+            builder.Append(value);
+            return this;
+        }
+
+        public SourceCodeStringBuilder AppendLine(string value)
+        {
+            builder.AppendLine(value);
+            return this;
+        }
+
+        public SourceCodeStringBuilder AppendLine()
+        {
+            builder.AppendLine();
+            return this;
+        }
+
+        public SourceCodeStringBuilder AppendNewLines(int numberOfNewLines)
+        {
+
+            for (int i = 0; i < numberOfNewLines; i++)
+            {
+                builder.AppendLine();
+            }
+            return this;
+        } 
+
+        public SourceCodeStringBuilder AddIndent()
+        {
+            currentIndent += stepsPerIndent;
+            return this;
+        }
+
+        public SourceCodeStringBuilder RemoveIndent()
+        {
+            currentIndent = currentIndent[0..^stepsPerIndent.Length];
+            return this;
+        }
+
+        public int Length => builder.Length;
+
+        public SourceCodeStringBuilder Insert(int index, string value)
+        {
+            builder.Insert(index, value);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return builder.ToString();
+        }
+    }
+}
